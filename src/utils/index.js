5334242675c24d4012a8744c4131f81fe1987472
Kali <kali@kali.kali>
2025-09-04 13:53:54 +0530

    fix sign-cookie file name and http-options

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,6 +1,6 @@
 import ApiError from "./apiError.js";
 import logger from "./logger.js";
-import signCookie from "./signCooke.js";
+import signCookie from "./signCookie.js";
 import response from "./response.js";
 
 export { ApiError, logger, signCookie, response };
