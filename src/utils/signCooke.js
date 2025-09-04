21d5cdeae37ac69fc322bcf6bf7a7a3b3d742efd
Kali <kali@kali.kali>
2025-09-04 13:09:56 +0530

    fix sign-cookie - http-options, and expire time

--- src/utils/signCooke.js
+++ src/utils/signCooke.js
@@ -1,12 +1,17 @@
 import jwt from "jsonwebtoken";
 
+const oneDay = 1000 * 60 * 60 * 24;
+
 const signCookie = (res, secret, options = {}) => {
-  const token = jwt.sign(secret, process.env.JWT_SECRET, { expiresIn: "1d" });
+  const token = jwt.sign(secret, process.env.JWT_SECRET, {
+    expiresIn: parseInt(process.env.JWT_EXPIRES_IN) * oneDay || oneDay * 7,
+  });
 
   const httpOptions = {
     httpOnly: true,
-    sameSite: "none",
     secure: process.env.NODE_ENV === "production",
+    sameSite: "lax",
+    expiresIn: parseInt(process.env.JWT_EXPIRES_IN) * oneDay || oneDay * 7,
     ...options,
   };
 
